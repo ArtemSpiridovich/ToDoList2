1e28af275407c5fcd0f09f3868c725ab8e5b1796
ArtemSpiridovich <ArtemSpiridovich99@gmail.com>
2020-02-28 13:42:05 +0300

    Updates

---
+++ precache-manifest.9c8e291311f3a36ab63af6d190b33bc0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16e54fda9df80fb2b19ebbfec87763ff",
+    "url": "/ToDoList2/index.html"
+  },
+  {
+    "revision": "aaf28f63364d9043971d",
+    "url": "/ToDoList2/static/css/main.a1da7050.chunk.css"
+  },
+  {
+    "revision": "bbb98d22d039f2fa248a",
+    "url": "/ToDoList2/static/js/2.52de2383.chunk.js"
+  },
+  {
+    "revision": "aaf28f63364d9043971d",
+    "url": "/ToDoList2/static/js/main.3236bca9.chunk.js"
+  },
+  {
+    "revision": "12c97a44187a9ba348a2",
+    "url": "/ToDoList2/static/js/runtime~main.a45e3aa1.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/ToDoList2FullVersion/precache-manifest.61e47a2e524cb3e1df70850019c797fe.js"
+  "/ToDoList2/precache-manifest.9c8e291311f3a36ab63af6d190b33bc0.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ToDoList2FullVersion/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ToDoList2/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
